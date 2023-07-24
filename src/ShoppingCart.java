8520f4fee6fb816121988c47ddd7be67c0bc8ea9
OmPrakash Kuswaha <okuswaha@routeone.com>
2023-07-23 20:17:32 -0400

    added new properties to Shopping Cart

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,4 +1,7 @@
 class ShoppingCart {
+    String cartId;
+    String cartName;
+    String cartDescription;
     public static void main(String args) {
         System.out.println("Implemented Shopping Cart");
     }
